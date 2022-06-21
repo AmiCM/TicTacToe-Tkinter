d295c1ad5c71b891778d2ccdb85eb899f8f02d58
AmiCM <amicampos2001@hotmail.com>
2022-06-21 15:13:46 -0500

    Frames Distribution

--- main.py
+++ main.py
@@ -1,54 +1,37 @@
 import tkinter as tk
 from PIL import ImageTk
-import sqlite3
-from numpy import random
-import pyglet
+import settings
+import utils
 
-# initialize app
+# Create Window
 root = tk.Tk()
-root.title("Recipe Picker")
-bg_colour="#3d6466"
+root.configure(bg=settings.BG_COLOUR)
+root.title("Tic Tac Toe Game")
+root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
 
-def load_frame2():
-    print("Hello Marya")
+##root.eval("tk::PlaceWindow . center")
+##x = root.winfo_screenwidth() // 2
+##y = int(root.winfo_screenheight() * 0.1)
+##root.geometry('500x600+' + str(x) + '+' + str(y))
 
-## root.eval("tk::PlaceWindow . center")
-x = root.winfo_screenwidth() // 2
-y = int(root.winfo_screenheight() * 0.1)
-root.geometry('500x600+' + str(x) + '+' + str(y))
+# Frames
+top_frame = tk.Frame(root,
+                     width=utils.width_percentage(100),
+                     height=utils.height_percentage(20),
+                     bg="red")
+top_frame.place(x=0,y=0)
 
-# Create a Frame Widget
-frame1 = tk.Frame(root, width=500, height=600, bg=bg_colour)
-frame1.grid(row=0, column=0)
-frame1.pack_propagate(False)
+mid_frame = tk.Frame(root,
+                     width=utils.width_percentage(100),
+                     height=utils.height_percentage(20),
+                     bg="blue")
+mid_frame.place(x=0,y=utils.height_percentage(20))
 
-# Frame 1 Widgets
-logo_img = ImageTk.PhotoImage(file="assets/RRecipe_logo.png")
-logo_widget = tk.Label(frame1, image=logo_img, bg=bg_colour)
-logo_widget.image = logo_img
-logo_widget.pack()
-
-tk.Label(
-    frame1,
-    text="Random Recipe",
-    bg=bg_colour,
-    fg="white",
-    font=("TkMenuFont",14)
-).pack()
-
-# Button widget
-tk.Button(
-    frame1,
-    text="shuffle",
-    font=("TkHeadingFont",20),
-    bg="#28393a",
-    fg="white",
-    cursor="hand2",
-    activebackground="#badee2",
-    activeforeground="black",
-    command=lambda:load_frame2()
-).pack(pady=20)
-
+low_frame = tk.Frame(root,
+                     width=utils.width_percentage(100),
+                     height=utils.height_percentage(60),
+                     bg="green")
+low_frame.place(x=0,y=utils.height_percentage(40))
 
 # run app
 root.mainloop()
---
+++ utils.py
@@ -0,0 +1,7 @@
+import settings
+
+def height_percentage(percentage):
+    return settings.HEIGHT * percentage / 100
+
+def width_percentage(percentage):
+    return settings.WIDTH * percentage / 100
